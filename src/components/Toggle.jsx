2bbaec1bd71d8acc6c6cc828f4d94ab3ed22c76c
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-09-11 01:42:44 +0530

    save

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import styles from './toggle.module.css'
+import styles from './toggle.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Toggle = () => {
   return (
-    <div className={styles.body}>
-    <input type="checkbox" className={styles.checkbox} id="checkbox"/>
-  <label for="checkbox" class={ styles.label}>
+//     <div className={styles.body}>
+//     <input type="checkbox" className={styles.checkbox} id="checkbox"/>
+//   <label for="checkbox" class={ styles.label}>
     
-    <i className={styles.fa-moon} class="fas fa-moon"></i>
-    <i className={styles.fa-sun} class='fas fa-sun'></i>
-    <div className={styles.ball}/>
+//     {/* <i className={styles.fa-moon} class="fas fa-moon"></i>
+//     <i className={styles.fa-sun} class='fas fa-sun'></i> */}
+//     {/* <FontAwesomeIcon className={styles.fa-moon} icon="fas fa-moon" />
+//     <FontAwesomeIcon className={styles.fa-sun}  icon="fas fa-sun" /> */}
+//     <FontAwesomeIcon className={styles.fasun} icon="fa-solid fa-sun" />
+//     <FontAwesomeIcon className={styles.famoon} icon="fa-solid fa-moon" />
+//     <div className={styles.ball}/>
+//   </label>
+// </div>
+<div>
+    <input type="checkbox" class="checkbox" id="checkbox"/>
+  <label for="checkbox" class="label">
+    <i class="fas fa-moon"></i>
+    <i class='fas fa-sun'></i>
+   <div class='ball'/>
   </label>
 </div>
   )
