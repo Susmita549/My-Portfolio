cae7a8a651b3bae1b54b81a1dde7b42b04314069
Susmita Mandal <99960489+Susmita549@users.noreply.github.com>
2022-09-12 00:39:27 +0530

    all chamges done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,10 +5,10 @@ import Fade from "react-reveal/Fade";
 const project = [
   {youtubelink:"https://www.youtube.com/embed/OZo_ryRsYM4",cardTitle:"FRONTEND MASTER",cardText:"Frontend Master is a website for learning front-end courses. It provides a lots of free courses as well as paid courses.",github:"https://github.com/Susmita549/Frontend_Master",deploy:"https://darling-chebakia-d6a79c.netlify.app/",teckStack:"html,css,javascript"},
   {youtubelink:"https://www.youtube.com/embed/mqnzs3f0M2Y",cardTitle:"INTERN THEORY",cardText:"Intery Theory is a online learning platform. It provides different type of internships and job opportunity indifferent different location.",github:"https://github.com/Susmita549/Intern-Theory",deploy:"https://tranquil-sherbet-8f6bdd.netlify.app/",teckStack:"html,css,javascript"},
-  {youtubelink:"https://www.youtube.com/embed/mqnzs3f0M2Y",cardTitle:"MY NETDIARY",cardText:"MyNetDiary is a diet app that focuses on calorie tracking based on user information provided during signup.",github:"https://github.com/Susmita549/MyNetDiary_Clone",deploy:"https://mynetdiary-website-clone.netlify.app/",teckStack:"react,nodejs,express,mongodb,  chakra-ui"},
-  {youtubelink:"https://www.youtube.com/embed/mqnzs3f0M2Y",cardTitle:"SHOPBOB",cardText:"Shopbop is an online fashion store that carries a variety of brands and designers that range from affordable to luxury. ",github:"https://github.com/Susmita549/Shopbop-Clone-Project",deploy:"https://shopbop-clone-project.netlify.app/",teckStack:"react,redux,css"},
-  {youtubelink:"https://www.youtube.com/embed/mqnzs3f0M2Y",cardTitle:"BETTER JOTTER",cardText:"A Chrome extension to keep tasks as user specific. This is a perfect tool for taking quick & personal notes inside your browser with a single click. ",github:"https://github.com/Susmita549/BetterJotter",deploy:"https://betterjotter.netlify.app/",teckStack:"react,nodejs,express,mongodb,  chakra-ui"},
-  {youtubelink:"https://www.youtube.com/embed/mqnzs3f0M2Y",cardTitle:"PHARMEASY",cardText:"PharmEasy is an Indian e-pharmacy company that sells medicines, diagnostics and telehealth online.",github:"https://github.com/Susmita549/PharmEasy-Clone",deploy:"https://pharmeasy-clone-nem201.netlify.app/",teckStack:"react,redux,mongodb,express,  nodejs"}
+  {youtubelink:"https://www.youtube.com/embed/o6lIvfYbzdc",cardTitle:"MY NETDIARY",cardText:"MyNetDiary is a diet app that focuses on calorie tracking based on user information provided during signup.",github:"https://github.com/Susmita549/MyNetDiary_Clone",deploy:"https://mynetdiary-website-clone.netlify.app/",teckStack:"react,nodejs,express,mongodb,  chakra-ui"},
+  {youtubelink:"https://www.youtube.com/embed/xlhpwl_SEOc",cardTitle:"SHOPBOB",cardText:"Shopbop is an online fashion store that carries a variety of brands and designers that range from affordable to luxury. ",github:"https://github.com/Susmita549/Shopbop-Clone-Project",deploy:"https://shopbop-clone-project.netlify.app/",teckStack:"react,redux,css"},
+  {youtubelink:"https://www.youtube.com/embed/6BE7SdoZaLU",cardTitle:"BETTER JOTTER",cardText:"A Chrome extension to keep tasks as user specific. This is a perfect tool for taking quick & personal notes inside your browser with a single click. ",github:"https://github.com/Susmita549/BetterJotter",deploy:"https://betterjotter.netlify.app/",teckStack:"react,nodejs,express,mongodb,  chakra-ui"},
+  {youtubelink:"https://www.youtube.com/embed/dfyw0EVQkWk",cardTitle:"PHARMEASY",cardText:"PharmEasy is an Indian e-pharmacy company that sells medicines, diagnostics and telehealth online.",github:"https://github.com/Susmita549/PharmEasy-Clone",deploy:"https://pharmeasy-clone-nem201.netlify.app/",teckStack:"react,redux,mongodb,express,  nodejs"}
 ]
 const Projects = () => {
   return (
@@ -45,13 +45,13 @@ const Projects = () => {
                     </div>
                     <div className="container d-flex justify-content-between containerBox">
                       <button className="btn ">
-                        <a href={el.github}>
+                        <a href={el.github} target="_blank">
                           <span>Click Me!!</span>
                           <span>code</span>
                         </a>
                       </button>
                       <button className="btn ">
-                        <a href={el.deploy}>
+                        <a href={el.deploy} target="_blank">
                           <span>Click Me!!</span>
                           <span>Website</span>
                         </a>
